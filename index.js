e00e06fd7d7bc2508ad7055ebee8c75bc8e1099b
Max Stoiber <mxstbr@github.com>
2019-02-28 14:06:06 +0100

    CORS

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const { stateFromMarkdown } = require("draft-js-import-markdown");
 const { createError, json, text } = require("micro");
 
 module.exports = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
   if (req.method !== "POST") throw createError(405, "Please POST your data.");
 
   if (req.url === "/to") {
